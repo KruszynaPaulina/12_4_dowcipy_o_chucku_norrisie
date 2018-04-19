77bbd49fdfb6fa9098fe42b6ee2b69d095458bf8
Paulina Kruszyna <kruszyna.paulina@gmail.com>
2018-04-19 14:51:08 +0200

    Change pure javascript for jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 var url = 'http://api.icndb.com/jokes/random';
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function(){
-  getJoke();
+var $button = $('#get-joke').click(function() {
+	getJoke();
 });
 
-var paragraph = document.getElementById('joke');
+var $paragraph = $('#joke');
 
-function getJoke() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerHTML = response.value.joke;
-  });
-  xhr.send();
+	function getJoke() {
+		$.ajax({
+			method: 'GET',
+			url: url, 
+			success: function(res) {
+			$paragraph.text(res.value.joke);
+		}
+
+	});
 }
 
 getJoke();
